c35b50b37cce38354494e378b835467f110c7101
Rabby-khan-04 <ajrabbyk72@gmail.com>
2026-05-14 19:46:06 +0600

    DEBUGGING

--- src/lib/client/api-client.js
+++ src/lib/client/api-client.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
+console.log("BASE_URL:", BASE_URL);
+
 export const publicClient = axios.create({
   baseURL: BASE_URL,
   headers: {
